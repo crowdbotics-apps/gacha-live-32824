253ae19d84ea83772974b5d03d455d5dca25836e
Crowdbotics <team@crowdbotics.com>
2022-01-13 16:17:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gacha_live_32824/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gacha_live_32824;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gacha_live_32824";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gacha_live_32824",
+  url: "https://gacha_live_32824.botics.co",
+  api: "https://gacha_live_32824.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
